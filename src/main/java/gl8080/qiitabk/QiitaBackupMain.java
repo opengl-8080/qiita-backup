79f9f050c42eae87aedb7285ed214da8c9302925
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-02-11 19:28:24 +0900

    #2 qiita api を使って通信できた

--- src/main/java/gl8080/qiitabk/QiitaBackupMain.java
+++ src/main/java/gl8080/qiitabk/QiitaBackupMain.java
@@ -1,5 +1,16 @@
 package gl8080.qiitabk;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,7 +20,31 @@ public class QiitaBackupMain {
     
     public static void main(String[] args) {
         try (ConfigurableApplicationContext ctx = SpringApplication.run(QiitaBackupMain.class, args)) {
+            execute();
+        }
+    }
+    
+    private static void execute() {
+        try {
+            String token = new String(Files.readAllBytes(Paths.get("qiita-token")));
             
+            URL url = new URL("https://qiita.com/api/v2/authenticated_user");
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Authorization", "Bearer " + token);
+            
+            con.connect();
+            
+            try (InputStream in = con.getInputStream()) {
+                Path tmp = Paths.get("./tmp");
+                Files.copy(in, tmp, StandardCopyOption.REPLACE_EXISTING);
+                
+                try (Stream<String> s = Files.lines(tmp)) {
+                    s.forEach(System.out::println);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
